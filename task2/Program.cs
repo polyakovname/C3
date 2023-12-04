5b8e5fd70c30cce91a3c4c31b0dc322a03c41312
denis <denis.polyakov02@yandex.ru>
2023-12-04 15:13:47 +0300

    second

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте массив на 10 целых чисел.
+// Напишите программу, которая определяет
+// количество чётных чисел в массиве.
+// Пример
+// массив [6 7 19 34 3 1 4 7 9 1] => 3
+// массив [1 8 43 4 55 60 3 2 1 3] => 4
+
+int[] array = new int[10];
+int result = 0;
+for (int i=0; i<array.Length; i++)
+{
+    array[i] = new Random().Next(1, 100);
+    Console.Write(array[i]+" ");
+}
+Console.WriteLine();
+for (int i=0; i<array.Length; i++)
+{
+    if (array[i]%2 == 0)
+    {
+        result++;
+    }
+}
+Console.Write(result);
